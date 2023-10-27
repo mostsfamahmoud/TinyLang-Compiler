89ac06bc1258bf7b51d8102e94448caf11b63564
Habiba ahmed alaa eldin <pop82000@gmail.com>
2023-10-27 05:04:27 +0200

    GUI

---
+++ src/compilerproject/CompilerProject.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package compilerproject;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author habib
+ */
+public class CompilerProject extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+           Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+            Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Scanner");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+             } catch (IOException e) {
+            System.out.println("Exception in GUIMain -> Start Method:-" + e);
+            System.exit(0);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxapplication1;
+
+import compilerproject.CustomInput;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author habib
+ */
+public class FXMLDocumentController implements Initializable {
+   FileChooser fileChooser;
+   File input;
+
+
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private TextArea TextArea1;
+    @FXML
+    private TreeTableView<?> Table;
+    @FXML
+    private HBox myHBox;
+    @FXML
+    private Button btnBrowse;
+    @FXML
+    private Button btnScan;
+    @FXML
+    private Button btnClear;
+    @FXML
+    private TreeTableColumn<?, ?> Token;
+    @FXML
+    private TreeTableColumn<?, ?> Type;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+         // TODO
+         fileChooser = new FileChooser();
+          FileChooser.ExtensionFilter Filter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+          FileChooser.ExtensionFilter Filter2 = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+         fileChooser.getExtensionFilters().add(Filter);
+          fileChooser.getExtensionFilters().add(Filter2);
+    }    
+
+
+
+    @FXML
+    private void Scan(ActionEvent event) {
+    }
+
+    @FXML
+    private void Clear(ActionEvent event) {
+    }
+
+    @FXML
+    private void FileSelector(ActionEvent event) {
+           input = fileChooser.showOpenDialog(new Stage());
+        if (input != null ) {
+            InputStream orgInStream = System.in;
+            try {
+                System.setIn(new FileInputStream(input));
+            } catch (FileNotFoundException ee) {
+                ee.printStackTrace();
+            }
+            String xml = CustomInput.readString();
+            TextArea1.setText(xml);
+            CustomInput.close();
+            System.setIn(orgInStream);
+           
+        } else {  
+         JOptionPane.showMessageDialog(null, "You didn't select a file","Error",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    
+}
